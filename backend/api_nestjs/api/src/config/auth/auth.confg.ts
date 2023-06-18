fb6928ff4c47f7b64a0d0f941c345d6f9ae49f24
Greedwy-Blu <greedwyblu@gmail.com>
2023-06-17 22:05:41 -0300

    update

---
+++ backend/api_nestjs/api/src/config/auth/auth.confg.ts
@@ -0,0 +1,11 @@
+export const authConfig = {
+  jwt: {
+    secret: 'crefdvrr3231568852d30950acdd976',
+    signOptions: {
+      expiresIn: '7d',
+    },
+  },
+  auth: {
+    saltRounds: 8,
+  },
+};
